6014318d322d9b5b3020696796b7d08cc735c581
Gian Carlo I. Gilos <gianCarlo.gilos@outlook.com>
2016-08-21 19:00:03 +0800

    Implemented ItemRegistry interface in InMemoryItemRegistry

--- PointOfSale/InMemoryItemRegistry.cs
+++ PointOfSale/InMemoryItemRegistry.cs
@@ -29,13 +29,7 @@ namespace PointOfSale.Domain
 
 		public IEnumerable<Item> getAvailableItems()
 		{
-			var items = new List<Item>();
-			items.Add(new Item("123456", "Bowl", 12.50));
-			items.Add(new Item("900000", "Phone", 7.50));
-			items.Add(new Item("456789", "Crab", 24.50));
-			items.Add(new Item("345670", "Plunger", 6.50));
-			items.Add(new Item("789010", "Fish", 10.25));
-			return items;
+			return items.Values.ToList();
 		}
 
 	}
